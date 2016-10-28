eea97d0509219bd9c8a8a86453df14256566c4ee
mohayonao <mohayonao@gmail.com>
2016-10-29 07:27:39 +0900

    clean up code

--- src/client/components/LConMatrix.js
+++ src/client/components/LConMatrix.js
@@ -1,5 +1,7 @@
 import React, { Component, PropTypes } from "react";
-import LConRow from "./LConRow";
+import LConKnob from "./LConKnob";
+import LConPadRect from "./LConPadRect";
+import { toCX, toCY } from "../designer";
 
 export default class LConMatrix extends Component {
   static propTypes = {
@@ -20,5 +22,37 @@ export default class LConMatrix extends Component {
     });
 
     return (<g className="lcon-matrix">{ elems }</g>);
+  }
+}
+
+export class LConRow extends Component {
+  static propTypes = {
+    row          : PropTypes.number.isRequired,
+    data         : PropTypes.arrayOf(PropTypes.number).isRequired,
+    onValueChange: PropTypes.func.isRequired,
+  };
+
+  shouldComponentUpdate(nextProp) {
+    return nextProp.data !== this.props.data;
+  }
+
+  render() {
+    const { row, data } = this.props;
+    const elems = data.map((colData, col) => {
+      const cx = toCX(col);
+      const cy = toCY(row);
+      const onValueChange = this.props.onValueChange.bind(null, col);
+      const LConCol = row === 2 ? LConPadRect : LConKnob;
+
+      if (row === 2) {
+        colData = ((colData & 0x30) >> 2) + (colData & 0x03);
+      }
+
+      return (
+        <LConCol key={ col } cx={ cx } cy={ cy } data={ colData } onValueChange={ onValueChange }/>
+      );
+    });
+
+    return (<g className="lcon-row">{ elems }</g>);
   }
 }
